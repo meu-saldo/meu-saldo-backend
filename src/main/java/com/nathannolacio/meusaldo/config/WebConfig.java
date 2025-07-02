060b55e9c43ba2a18f870adf21893941f561f18f
nathannolacio <nathannolacio04@gmail.com>
2025-07-02 16:26:36 -0300

    :wrench: confg: Incluindo o dominio da vercel para permitir enviar requisicoes

--- src/main/java/com/nathannolacio/meusaldo/config/WebConfig.java
+++ src/main/java/com/nathannolacio/meusaldo/config/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://meu-saldo-frontend.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
